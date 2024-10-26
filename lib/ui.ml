647bf6b54b966474b2907fb948268a9268d041b2
Anirudh Tenneti <at863@cornell.edu>
2024-10-26 13:29:19 -0400

    consolidate board ops

--- lib/ui.ml
+++ lib/ui.ml
@@ -1,39 +1,42 @@
-type tile =
-  | Empty
-  | Number of int
+open Board
 
-type grid = tile array array
+let intarrayarray_to_grid i = Array.map (Array.map create_tile) i
 
-let int_to_tile_value i = if i < 0 then Empty else Number i
-let intarray_to_grid i = Array.map (Array.map int_to_tile_value) i
-
-let print_grid (g : tile array array) =
-  let h = Array.length g in
-  let w = Array.length g.(0) in
-  print_char '*';
+(**For best results, g has at least one row and one column and is rectangular
+   (all rows have the same number of columns)*)
+let grid_to_string g =
+  let ints = Board.to_intarrayarray g in
+  let h = Array.length ints in
+  let w = Array.length ints.(0) in
+  let acc = ref "" in
+  acc := !acc ^ "*";
   for c = 0 to w - 1 do
-    print_string "---*"
+    acc := !acc ^ "---*"
   done;
-  print_endline "";
+  acc := !acc ^ "\n";
   for r = 0 to h - 1 do
-    print_char '|';
-    for c = 0 to w - 1 do
-      match g.(r).(c) with
-      | Number n ->
-          let s = string_of_int n in
-          print_string
-            ((match String.length s with
-             | 0 -> "   "
-             | 1 -> " " ^ s ^ " "
-             | 2 -> " " ^ s
-             | _ -> s)
-            ^ "|")
-      | Empty -> print_string "|   |"
+    acc := !acc ^ "|";
+    for c = 0 to Array.length ints.(r) - 1 do
+      let s = string_of_int ints.(r).(c) in
+      acc :=
+        if ints.(r).(c) < 0 then !acc ^ "   |"
+        else
+          !acc
+          ^ (match String.length s with
+            | 0 -> "   "
+            | 1 -> " " ^ s ^ " "
+            | 2 -> " " ^ s
+            | 3 -> s
+            | _ -> String.sub s 0 3)
+          ^ "|"
     done;
-    print_endline "";
-    print_char '*';
+    acc := !acc ^ "\n";
+    acc := !acc ^ "*";
     for c = 0 to w - 1 do
-      print_string "---*"
+      acc := !acc ^ "---*"
     done;
-    print_endline ""
-  done
+    acc := !acc ^ "\n"
+  done;
+  !acc
+
+let print_grid g = print_endline (grid_to_string g)
--- lib/ui.mli
+++ lib/ui.mli
@@ -4,11 +4,11 @@ type tile
 type grid
 (**The type of a grid of tiles*)
 
-val int_to_tile_value : int -> tile
+val int_to_tile : int -> tile
 (**Turn an int into a tile*)
 
-val intarray_to_grid : int array array -> grid
-(**Make a grid of tiles from an int array*)
+val intarrayarray_to_grid : int array array -> grid
+(**Make a grid of tiles from an int array array*)
 
 val print_grid : grid -> unit
 (**Print a user-friendly representation of a grid*)
