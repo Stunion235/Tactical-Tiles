b5012fd797b3f718a6d83cf0603b92589cd6e0af
Elli <emb427@cornell.edu>
2024-11-16 12:47:46 -0500

    made in_bound function

--- lib/board.ml
+++ lib/board.ml
@@ -50,6 +50,7 @@ let fill_board currBoard =
 
 let in_bound row column board =
   row <= Array.length board && column <= Array.length board.(0)
+
 (* let move_tile grid direction = let ex, ey = find_empty grid in match
    direction with | "W" | "w" -> failwith "TODO" | "A" | "a" -> failwith "TODO"
    | "S" | "s" -> failwith "TODO" | "D" | "d" -> failwith "TODO" | _ -> failwith
